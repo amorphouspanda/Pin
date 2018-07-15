c4f76a870c86271934af32ae719f67e782fdf293
Xelliese <32050930+Xelliese@users.noreply.github.com>
2018-07-15 06:50:16 -0700

    testing

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const
 	express = require('express'),
 	bodyParser = require('body-parser'),
-	convert = require('node_modules/convert-svg-to-png'),
-	converters = require('converters.js');
 	app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
@@ -18,6 +16,45 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 app.get('/', function (req, res) {
 	res.send("HELLO WORLD KILL ME NOW");
 })
+
+
+// Creates the endpoint for our webhook 
+app.post('/webhook/', (req, res) => {  
+ 
+	let body = req.body;
+
+	// Checks this is an event from a page subscription
+	if (body.object === 'page') {
+
+		// Iterates over each entry - there may be multiple if batched
+		body.entry.forEach(function(entry) {
+
+			// Gets the body of the webhook event
+			let webhook_event = entry.messaging[0];
+			console.log(webhook_event);
+
+			// Get the sender PSID
+			let sender_psid = webhook_event.sender.id;
+			console.log('Sender PSID: ' + sender_psid);
+
+			// Check if the event is a message or postback and
+			// pass the event to the appropriate handler function
+			if (webhook_event.message) {
+				handleMessage(sender_psid, webhook_event.message);        
+			} else if (webhook_event.postback) {
+				handlePostback(sender_psid, webhook_event.postback);
+			}
+  
+		});
+
+		// Returns a '200 OK' response to all requests
+		res.status(200).send('EVENT_RECEIVED');
+	} else {
+		// Returns a '404 Not Found' if event is not from a page subscription
+		res.sendStatus(404);
+	}
+
+});
 
 
 // Adds support for GET requests to our webhook
@@ -49,54 +86,8 @@ app.get('/webhook/', (req, res) => {
 });
 
 
-// Creates the endpoint for our webhook 
-app.post('/webhook/', (req, res) => {  
- 
-	let body = req.body;
-
-	// Checks this is an event from a page subscription
-	if (body.object === 'page') {
-
-		// Iterates over each entry - there may be multiple if batched
-		body.entry.forEach(function(entry) {
-
-			// Gets the body of the webhook event
-			let webhook_event = entry.messaging[0];
-			console.log(webhook_event);
-
-			// Get the sender PSID
-			let sender_psid = webhook_event.sender.id;
-			console.log('Sender PSID: ' + sender_psid);
-
-			// Check if the event is a message or postback and
-			// pass the event to the appropriate handler function
-			if (webhook_event.message) {		// LOTS OF QUESTIONS
-				let pngBuffer = inputToPng(sender_psid, webhook_event.message);  
-				let url = 'https://mobile-latex.herokuapp.com/' + sender_psid + '/';
-				
-				res.writeHead(200,
-					{Location: url}
-				);
-				
-				res.set('Content-Type', 'image/png');
-				res.send(pngBuffer);
-				
-				callSendAPI(sender_psid, url);
-			}
-		});
-
-		// Returns a '200 OK' response to all requests
-		res.status(200).send('EVENT_RECEIVED');
-	} else {
-		// Returns a '404 Not Found' if event is not from a page subscription
-		res.sendStatus(404);
-	}
-
-});
-
-
-// converts to png
-function inputToPng(received_message) {
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
 
 	let response;
 
@@ -105,13 +96,59 @@ function inputToPng(received_message) {
 
 		// Create the payload for a basic text message
 		response = {
-			let input = ${received_message.text};
-			let svg = texToSvgBuffer(input);
-			let png = convert(svg);
-			
-			return png;
+			"text": `You sent the message: "${received_message.text}". Now send me an image!`
 		}
+	} else if (received_message.attachments) {
+		
+		// Get the URL of the message attachment
+		let attachment_url = received_message.attachments[0].payload.url;
+		response = {
+			"attachment": {
+				"type": "template",
+				"payload": {
+					"template_type": "generic",
+					"elements": [{
+						"title": "Is this the right picture?",
+						"subtitle": "Tap a button to answer.",
+						"image_url": attachment_url,
+						"buttons": [
+							{
+								"type": "postback",
+								"title": "Yes!",
+								"payload": "yes",
+								},
+							{
+								"type": "postback",
+								"title": "No!",
+								"payload": "no",
+							}
+						],
+					}]
+				}
+			}
+		}
+	}  
+
+	// Sends the response message
+	callSendAPI(sender_psid, response);    
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+	let response;
+
+	// Get the payload for the postback
+	let payload = received_postback.payload;
+
+	// Set the response based on the postback payload
+	if (payload === 'yes') {
+		response = { "text": "Thanks!" }
+	} else if (payload === 'no') {
+		response = { "text": "Oops, try sending another image." }
 	}
+	
+	// Send the message to acknowledge the postback
+	callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
