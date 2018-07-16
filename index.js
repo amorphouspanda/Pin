70266d06fa021ad6b1e8b235cab15947863ddd50
Xelliese <32050930+Xelliese@users.noreply.github.com>
2018-07-16 03:29:08 -0700

    testing

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/webhook/', (req, res) => {
 		});
 		
 		// Returns a '200 OK' response to all requests
-		res.status(200).write('EVENT_RECEIVED');
+		res.status(200).send('EVENT_RECEIVED');
 	} else {
 		// Returns a '404 Not Found' if event is not from a page subscription
 		res.sendStatus(404);
